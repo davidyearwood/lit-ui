f06692fbf639c1d1555e1d86860bee4912188937
Davidood <dwhywood@gmail.com>
2019-01-06 23:17:57 -0600

    Configure remote redux dev tools for debugging redux

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+/* eslint-disable no-unused-vars */
+import DevToolsApp from "remotedev-app";
 
 AppRegistry.registerComponent(appName, () => App);
