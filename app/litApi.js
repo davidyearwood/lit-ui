8edfe9b05839ddec6355dee921d0ba3b62fd8101
Kremor Pärt <kremor.part@gmail.com>
2019-01-09 08:29:23 -0600

    Library to connect with API

---
+++ app/litApi.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const API_URL = "https://litapi.projectunicorn.net/api/";
+
+export default {
+  getLocations(lat, lng, radius = 10000) {
+    const url = API_URL + "locations/";
+    return axios
+      .get(url, {
+        params: {
+          lat: lat,
+          lng: lng,
+          radius: radius
+        }
+      })
+      .then(response => {
+        return response.data;
+      })
+      .catch(error => {
+        return Promise.reject(error);
+      });
+  },
+
+  setDeviceLocation(devicdeId, locationId) {
+    const url = API_URL + "set-device-location/";
+    axios
+      .post(url, {
+        id: devicdeId,
+        place_id: locationId
+      })
+      .then(response => {
+        return response.data;
+      })
+      .catch(error => {
+        return Promise.reject(error);
+      });
+  }
+};
