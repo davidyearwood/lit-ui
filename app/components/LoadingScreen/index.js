7c817d84a255b963210fc87d09517202d9b4f2b8
Kremor Pärt <kremor.part@gmail.com>
2019-02-08 14:41:39 -0600

    LoadingScreen Modified.

--- app/components/LoadingScreen/index.js
+++ app/components/LoadingScreen/index.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { StatusBar, StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View
+} from "react-native";
+import Logo from "../SVG/Logo.js";
 
 const styles = StyleSheet.create({
   loadingScreen: {
@@ -9,16 +16,35 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     marginTop: StatusBar.currentHeight
   },
+  logo: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+    height: 60,
+    width: 60
+  },
+  spinnerContainer: {
+    alignItems: "center",
+    justifyContent: "center"
+  },
   text: {
     color: "#fff",
-    fontSize: 30,
-    fontWeight: "bold"
+    fontSize: 18,
+    fontWeight: "normal",
+    marginTop: 30,
+    textAlign: "center"
   }
 });
 
 const LoadingScreen = () => (
   <View style={styles.loadingScreen}>
-    <Text style={styles.text}>Loading</Text>
+    <ActivityIndicator size={80} color="#fff" />
+    <Text style={styles.text}>
+      Good parties create a temporary youthfulness
+    </Text>
+    <View style={styles.logo}>
+      <Logo height={60} width={60} />
+    </View>
   </View>
 );
 
